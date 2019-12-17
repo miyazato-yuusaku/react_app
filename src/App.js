7547a58553ccee74d54c206aa2e3f161d5795c57
yuusaku <yuusaku@example.com>
2019-12-17 16:08:33 +0900

    まだ保留

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react'
 
-function Greeting () {
-  const r = Math.random(Math.random() * 3)
-  if (r === 0) return <p>今日もがんばりましょう</p>
-  if (r === 1) return <p>今日もがんばりましょう</p>
-  if (r === 2) return <p>今日もがんばりましょう</p>
-}
 
 class App extends React.Component() {
   constructor (props) {
@@ -15,9 +9,7 @@ class App extends React.Component() {
 
   render () {
     return (
-      <div>
-        <Greeting />
-      </div>
+      <div />
     )
   }
 }
