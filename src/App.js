2e69f68e3ff732047659e672669bf8a0fb9ad506
yuusaku <yuusaku@example.com>
2019-12-18 22:24:10 +0900

    完成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react'
 
-const Greeting = props => {
-  return (
-    <div>
-      <h1>{props.value}</h1>
-        <p><img src={props.src} /></p>
-    </div>
-  )
-}
-
 class App extends React.Component {
   constructor (props) {
     super(props)
-    this.state = {
-    }
+    this.state = {}
   }
 
   render () {
     return (
       <div>
-        <Greeting value='書道' src='https://original-sho.com/jetb/wp-content/uploads/2019/04/IMG_20190401_114855_261.jpg' />
+        <p>探すのに時が有り</p>
+        <p>捨てるのに時がある</p>
       </div>
     )
   }
